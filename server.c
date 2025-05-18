83baf2ffd95cab2356ac89cc94434902cb68322e
Aditya Nair <adinair0206@gmail.com>
2025-05-17 20:58:39 -0400

    Completed basic web server layout

--- server.c
+++ server.c
@@ -2,15 +2,24 @@
 #include <sys/socket.h>
 //  Header file with all the methods that help define socket configurations
 #include <netinet/in.h>
+//  Standard Libraries imports
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-//  The socket we create
+//  The socket we create for the basic definition of a socket
 int server_fd;
+//  The socket that is used to listen to accept connections, read and write data, then closed
+int new_socket;
 //  Struct to define the socket configurations
 struct sockaddr_in address;
+//  int to store the size of the address
+int addrlen = sizeof(address);
 //  PORT Number of the socket
-const int PORT = 8080;
+#define PORT 8080
 
-int main(){
+int main(int argc, const char** argv){
     /*  AF_INET for using IPv4
         SOCK_STREAM for using TCP/IP 
         0 for using IP as the protocol
@@ -18,7 +27,7 @@ int main(){
     if ((server_fd = socket(AF_INET,SOCK_STREAM,0)) < 0)
     {
         perror("Cannot create socket");
-        return 0;
+        exit(EXIT_FAILURE);
     }
 
 
@@ -34,8 +43,41 @@ int main(){
     //  Bind the socket configurations to the socket
     if (bind(server_fd,(struct sockaddr *)&address,sizeof(address)) < 0){
         perror("bind failed");
-        return 0;
+        exit(EXIT_FAILURE);
     }
 
-    
+    //  Listen tells the socket to accept connections with a backlog of 3 processes in the queue
+    if (listen(server_fd, 3) < 0){
+        perror("Listen Failed");
+        exit(EXIT_FAILURE);
+    }
+
+    //  Constantly accept connections, read and write data before closing the connection
+    while(1){
+        printf("-------ACCEPTING CONNECTIONS--------\n");
+        //  Create a new socket based on the server_fd socket configurations to accept connections
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0){
+            perror("Accept Failed");
+            exit(EXIT_FAILURE);
+        }
+        
+        //  Read data and store it in a char buffer
+        printf("-------READING DATA-------\n");
+        char buffer[3000] = {0};
+        long readData = read(new_socket,buffer,3000);
+        //  Print the data
+        printf("%s\n",buffer);
+
+        //  Write data 
+        printf("-------WRITING DATA--------\n");
+        const char* message = "Hello From AD's Web Server!";
+        write(new_socket,message,strlen(message));
+
+        //  Close the socket
+        printf("-------CLOSING SOCKET--------\n");
+        close(new_socket);
+    }
+
+    //  Exit
+    return 0;
 }
